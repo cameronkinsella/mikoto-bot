1a0fa1cdb93ceee4c5962a86f87c233683b417d5
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2023-02-13 00:03:39 -0500

    Start work on LSMDSO driver

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,59 @@
 #![no_main]
 #![no_std]
 
+use cortex_m_rt::entry;
 use defmt_rtt as _;
 use panic_probe as _;
 
-#[rtic::app(device = mikoto_bot::pac, peripherals = true)]
-mod app {
-    use core::fmt::Write;
-    use mikoto_bot::{
-        hal::{gpio::Edge, prelude::*},
-        pac, Button, Led, Usart,
-    };
+use lsmdso::{GyroscopeFullScale, GyroscopeOutput, Lsm6dso};
+use mikoto_bot::{
+    hal::{i2c::I2c, prelude::*},
+    pac,
+};
 
-    #[shared]
-    struct Resources {
-        button: Button,
-        led: Led,
-        usart: Usart<pac::USART2>,
-    }
+const ADDR: u8 = 0x6B;
 
-    #[local]
-    struct Local {}
+#[entry]
+fn main() -> ! {
+    // The Stm32 peripherals
+    let dp = pac::Peripherals::take().unwrap();
 
-    #[init]
-    fn init(ctx: init::Context) -> (Resources, Local, init::Monotonics) {
-        // Device specific peripherals
-        let mut dp = ctx.device;
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.use_hse(8.MHz()).freeze();
+    let mut delay = dp.TIM5.delay_us(&clocks);
+    let mut _counter = dp.TIM2.counter_us(&clocks);
 
-        // Setup the system clock
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
+    let gpiob = dp.GPIOB.split();
+    let scl = gpiob.pb8.into_alternate_open_drain();
+    let sda = gpiob.pb9.into_alternate_open_drain();
 
-        let mut syscfg = dp.SYSCFG.constrain();
+    let i2c = I2c::new(dp.I2C1, (scl, sda), 100.kHz(), &clocks);
 
-        let gpioa = dp.GPIOA.split();
-        let gpioc = dp.GPIOC.split();
+    let mut lsm = Lsm6dso::new(i2c, ADDR).unwrap();
 
-        // Setup Button and enable external interrupt
-        let mut button = Button::new(gpioc.pc13);
-        button.enable_interrupt(Edge::Rising, &mut syscfg, &mut dp.EXTI);
+    lsm.set_gyroscope_scale(GyroscopeFullScale::Dps125).unwrap();
+    lsm.set_gyroscope_output(GyroscopeOutput::Rate12_5).unwrap();
 
-        // Setup the led
-        let led = Led::new(gpioa.pa5);
+    let mut vals: [(f32, f32, f32); 1500] = [lsm.read_gyro().unwrap(); 1500];
+    loop {
+        vals.fill_with(|| {
+            delay.delay_ms(80u32);
+            lsm.read_gyro().unwrap()
+        });
+        let mut x_sum: f64 = 0.0;
+        let mut y_sum: f64 = 0.0;
+        let mut z_sum: f64 = 0.0;
+        for (x, y, z) in vals {
+            x_sum += x as f64;
+            y_sum += y as f64;
+            z_sum += z as f64;
+        }
 
-        // Configure sender for USART2
-        let mut usart = Usart::new(gpioa.pa2, dp.USART2, &clocks);
-        writeln!(usart.tx(), "init done").unwrap();
-
-        (
-            Resources { led, button, usart },
-            Local {},
-            init::Monotonics(),
-        )
-    }
-
-    #[idle]
-    fn idle(_ctx: idle::Context) -> ! {
-        // The idle loop
-        loop {}
-    }
-
-    #[task(binds = EXTI15_10, shared = [led, button, usart])]
-    fn on_button_press(ctx: on_button_press::Context) {
-        let mut led = ctx.shared.led;
-        let mut button = ctx.shared.button;
-        let mut usart = ctx.shared.usart;
-
-        // Clear the interrupt
-        button.lock(|b| b.clear_interrupt_pending_bit());
-
-        // Toggle the led
-        led.lock(|l: &mut Led| l.toggle());
-
-        usart.lock(|u| writeln!(u.tx(), "Button pressed!").unwrap());
+        defmt::info!(
+            "x: {}, y: {}, z: {}",
+            x_sum / vals.len() as f64,
+            y_sum / vals.len() as f64,
+            z_sum / vals.len() as f64
+        );
     }
 }
